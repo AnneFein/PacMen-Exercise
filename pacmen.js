561bbaf34bd6154af09cdd812bd89f15b9aba38a
AnneFein <145065711+AnneFein@users.noreply.github.com>
2023-12-04 12:52:01 -0600

    fixed image file names in pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,8 +1,8 @@
 var pos = 0;
 let pageWidth = window.innerWidth;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['pacman1.png', 'pacman2.png'],
+  ['pacman3.png', 'pacman4.png'],
 ];
 var direction = 0;
 var focus = 0;
@@ -27,6 +27,3 @@ if (direction == 0 && pos + imgWidth > pageWidth) direction = 1;
 if (direction == 1 && pos < 0) direction = 0;
   return direction;
 }
-
-//Please do not change
-module.exports = checkPageBounds;
